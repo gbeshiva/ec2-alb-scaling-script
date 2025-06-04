1bf19203fe9c8620e25c937aafc76a1d1aa89358
gbeshiva <76791428+gbeshiva@users.noreply.github.com>
2025-06-04 11:17:27 +0530

    Create veriables.tf

---
+++ infra/veriables.tf
@@ -0,0 +1,16 @@
+variable "ami_id" {
+  description = "default vpc id"
+  type        = string
+  default     = "ami-0f535a71b34f2d44a"
+}
+variable "instance_type" {
+  description = "default vpc id"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "vpc_availability_zones" {
+  type        = list(string)
+  description = "Availability Zones"
+  default     = ["ap-south-1a", "ap-south-1b"]
+}
